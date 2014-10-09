c74510268a7687adafca3d5a7a4a7224eb1cfa4d
e.berbecaru@gmail.com <e.berbecaru@gmail.com>
2014-10-09 13:50:30 +0200

    Initial Commit

---
+++ Pedometer/Pedometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pedometer
+//
+//  Created by Eduard Berbecaru on 09/10/14.
+//  Copyright (c) 2014 FH-Hagenberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
