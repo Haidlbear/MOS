e91bb943c99f04100c769846cea26955f5b03cb6
dan <dan-n@gmx.at>
2014-10-31 11:00:47 +0100

    t

--- Pedometer/Pedometer/AppDelegate.swift
+++ Pedometer/Pedometer/AppDelegate.swift
@@ -34,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        app.lastTimerTimeStamp = CACurrentMediaTime()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
@@ -43,14 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        
-        app.timerTimeStamp = CACurrentMediaTime()
-        app.timerSeconds += Int (app.timerTimeStamp - app.lastTimerTimeStamp)
-        println(app.timerTimeStamp - app.lastTimerTimeStamp)
-
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+   
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
